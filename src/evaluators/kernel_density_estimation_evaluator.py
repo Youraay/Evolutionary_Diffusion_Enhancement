13ac8b24f7f6a35e6f048b5f56d67889059852b5
Youri Sinziri <youri@sinziri.de>
2025-10-08 16:40:53 +0200

    Added PCA for Numeric Stability in KDE

--- src/evaluators/kernel_density_estimation_evaluator.py
+++ src/evaluators/kernel_density_estimation_evaluator.py
@@ -6,13 +6,14 @@ import torch
 
 from src.evaluators.base_evaluator import Evaluator
 from src.huggingface_models.image_embedding.blip2_embedding import Blip2EmbeddingModel
-
+from src.utils.pca import PCA
 
 class KernelDensityEstimationEvaluator(Evaluator):
     def __init__(self,
                  prompt: str,
                  bandwidth: float  =1.0,
                  kernel : str ='gaussian',
+                 K : int =128
 
 
 
@@ -20,15 +21,32 @@ class KernelDensityEstimationEvaluator(Evaluator):
         self.bandwidth: float = bandwidth
         self.kernel: str = kernel
         self.prompt: str = prompt
+        self.K = K
         base_path = Path(os.environ['BASE_PATH'])
         metrics_path = Path(os.environ['BLIP_2_BASELINE'])
         self.metric_path  = base_path / metrics_path / self.prompt.replace(" ", "_")
-        data = [torch.load(p)  for p in self.metric_path.glob("*.pt")]
+        print(self.metric_path)
+        glob = self.metric_path.glob("*.pt")
+        data =[]
+        for p in glob:
+            dp =torch.load(
+                str(p),
+                map_location=torch.device("cuda"),
+                weights_only=False,
+                )
+            data.append(dp.pooler_output)
+        
+
         if len(data) == 0:
             raise FileNotFoundError(f"No metric found at {self.metric_path}")
+
+
         data_stack = torch.cat(data, dim=0)
+
+        self.pca = PCA(self.data+stack)
+        self.reduced_data_stack = PCA.reduce_embeddings(data_stack, self.K)
         self.kde = KernelDensity(kernel=kernel, bandwidth=bandwidth)
-        self.kde.fit(data_stack)
+        self.kde.fit(self.reduced_data_stack)
         self.name = "KernelDensityEstimation"
 
     def evaluate(self, embeds: torch.Tensor, *args, **kwargs) -> dict[str, float]:
@@ -39,8 +57,12 @@ class KernelDensityEstimationEvaluator(Evaluator):
 
     def evaluate_batch(self, embeds: List[torch.Tensor], *args, **kwargs) -> list[dict[str,float]]:
 
-        stack = torch.cat(embeds, dim=0)
-        log_prob = self.kde.score_samples(stack)
+        print(self.reduced_data_stack.size())
+        stack = torch.stack(embeds)
+        print(embeds[0].size())
+        print(stack.size())
+        self.reduced__stack = PCA.reduce_embeddings(stack, self.K)
+        log_prob = self.kde.score_samples(reduced__stack)
         novelty_scores: List[float] = log_prob.tolist()
 
         return [{"name": self.name, "score": log_prob} for log_prob in novelty_scores]
