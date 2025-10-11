4a1e437dd52c727b3d223f8b1a3f82b4cffc73cb
yourisinziri <s7009756@stud.uni-frankfurt.de>
2025-10-11 11:41:16 +0200

    add tensor loader class

---
+++ src/utils/loader.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import torch
+
+
+class Loader:
+    pass
+
+
+class TensorLoader(Loader):
+
+    def __init__(self, base_path: Path):
+
+        self.base_path = base_path
+
+    def load_file(self, path: Path, weights_only=True):
+        query = self.base_path / path.name
+        return torch.load(path, weights_only=weights_only)
+
+    def load_files(self, path: Path, search_filter: str = "*.pt",device : str= "cuda", weights_only=True) -> list[torch.Tensor]:
+
+        output =[ ]
+        query = self.base_path / path.name
+        assert query.is_dir(), f"{query} is not a directory"
+        glob = path.glob(search_filter)
+        for p in glob:
+            t = torch.load(
+                str(p),
+                map_location=torch.device(device),
+                weights_only=weights_only,
+            )
+            output.append(t)
+
+        return output
